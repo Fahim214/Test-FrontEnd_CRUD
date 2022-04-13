54a1f7c25a00119b234e1a58be49849415e2401f
Fahim214 <fahimtu456@gmail.com>
2022-04-13 11:20:43 +0700

    build navbar comp

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container} from "react-bootstrap"
+
+const Home = () => {
+  return (
+    <div>
+        <Container>
+        <h3>Ini adalah aplikasi pertama saya</h3>
+        </Container>
+    </div>
+  )
+}
+
+export default Home
--- server/server.js
+++ server/server.js
@@ -1,25 +1,26 @@
 import express from "express";
 import dotenv from "dotenv"
-// import morgan from "morgan";
-import path from "path"
+import connectDatabase from "./config/db.js";
+
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 
 import productRoutes  from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
-import uploadRoutes from "./routes/uploadRoutes.js"
 
 
 const app = express()
 
 dotenv.config({ path: 'server/config/config.env' })
 
-import connectDatabase from "./config/db.js";
-
-
 connectDatabase()
+
+app.use(express.json())
 
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
-app.use("/api/upload", uploadRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5600
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
