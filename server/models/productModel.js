9dfc1dbcb3b64c0b2998315a58d75533627d66ae
Fahim214 <fahimtu456@gmail.com>
2022-04-13 08:31:26 +0700

    productModel file success

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,5 +1,14 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const productSchema = mongoose.Schema({
+const productSchema = mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId },
+    nama: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: String, required: true }
+}, {
+    timestamps: true
+})
 
-// })
+const Product = mongoose.model("Product", productSchema)
+
+export default Product
